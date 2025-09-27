8c86636f8f2b06d7fa75b67de3491f98ace66ca2
Raj Safi <rajsafi@Rajs-MacBook-Air.local>
2025-09-27 13:15:48 -0400

    rb

--- DNSServer.py
+++ DNSServer.py
@@ -27,7 +27,7 @@ def webServer(port=13331):
       
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], 'rb')    #fill in start              #fill in end   )
+      f = open(filename[1:],)    #fill in start              #fill in end   )
       
       body = f.read
 
