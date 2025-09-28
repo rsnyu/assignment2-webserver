5f9f47c8121c4979ee349be43d5a03d4e902db36
Raj Safi <rajsafi@Rajs-MacBook-Air.local>
2025-09-27 22:22:30 -0400

    correction2

--- webServer.py
+++ webServer.py
@@ -29,43 +29,53 @@ def webServer(port=13331):
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
       f = open(filename[1:], 'rb')     #fill in start              #fill in end   )
-      
+      body = f.read()
       
 
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
       #Fill in start 
               
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b'HTTP/1.1 200 OK\r\n'
-      outputdata += b'Server: RsCloudServer\r\n'
-      outputdata += b'Connection: close\r\n'
-      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
-      outputdata += b"Content-Length: " + str(len(body)).encode() + b"\r\n"
-      outputdata += b"\r\n"
+      headers = (
+          b"HTTP/1.1 200 OK\r\n"
+          b"Content-Type: text/html; charset=UTF-8\r\n"
+          b"Server: RsCloudServer/1.0\r\n"
+          b"Connection: close\r\n"
+          + f"Content-Length: {len(body)}\r\n".encode('ascii')
+          + b"\r\n"
+      )
+
+      connectionSocket.sendall(headers + body)
 
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
  
       #Fill in end
                
-      for i in f: #for line in file
+      #for line in file
       #Fill in start - append your html file contents #Fill in end 
-        outputdata += bytes(i, 'utf-8')
+      
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
 
       # Fill in start
-      connectionSocket.sendall(outputdata)
 
       # Fill in end
         
       connectionSocket.close() #closing the connection socket
       
     except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      # Remember the format you used in the try: block!
-      #Fill in start
-      connectionSocket.sendall(b'HTTP/1.1 404 Not Found')
+      body = b"<!doctype html><html><body><h1>404 Not Found</h1></body></html>"
+      headers = (
+          b"HTTP/1.1 404 Not Found\r\n"
+          b"Content-Type: text/html; charset=UTF-8\r\n"
+          b"Server: RsCloudServer/1.0\r\n"
+          b"Connection: close\r\n"
+          + f"Content-Length: {len(body)}\r\n".encode('ascii')
+          + b"\r\n"
+      )
+
+      connectionSocket.sendall(headers + body)
       #Fill in end
 
 
@@ -80,5 +90,4 @@ def webServer(port=13331):
   #sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-
   webServer(13331)
